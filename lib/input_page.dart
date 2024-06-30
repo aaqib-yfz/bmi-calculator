8fe5179262ff2bbe3f396fa0ccde7dd858d80fbe
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-06-30 20:32:10 +0500

    siuu

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
-
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = 0xFFEB1555;
+const reusablecontColor = 0xFF1D1E33;
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -14,63 +16,72 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('BMI CALCULATOR')),
+          title: const Center(child: Text('BMI CALCULATOR')),
         ),
-        body: Column(
+        body:  Column(
           children: <Widget>[
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
+                  child: reusablecont(
+                    colour:Color(reusablecontColor),
+                    cardChild: Column(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text()
+                      ],
                     ),
                   ),
                 ),
                 Expanded(
-                    child: Container(
-                        margin: EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1D1E33),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ))),
+                  child: reusablecont(colour:Color(reusablecontColor),cardChild: Column(),),
+                )
               ],
             )),
-
-            Expanded(child: Expanded(
-                    child: Container(
-                        margin: EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1D1E33),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ))), )
-
-               ,Expanded(
+            Expanded(
+              child: Expanded(child: reusablecont(colour:Color(reusablecontColor),cardChild: Column())),
+            ),
+            Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
+                  child: reusablecont(colour:Color(reusablecontColor),cardChild: Column()),
                 ),
-                Expanded(
-                    child: Container(
-                        margin: EdgeInsets.all(15.0),
-                        decoration: BoxDecoration(
-                          color: Color(0xFF1D1E33),
-                          borderRadius: BorderRadius.circular(10.0),
-                        ))),
+                Expanded(child: reusablecont(colour:Color(reusablecontColor),cardChild: Column())),
               ],
             )),
-
+            Container(
+              color: Color(bottomContainerColor),
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
           ],
+        
         ));
+  }
+}
+
+class reusablecont extends StatelessWidget {
+  reusablecont({required this.colour, required this.cardChild});
+  final Color colour;
+  final Widget cardChild;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
   }
 }
