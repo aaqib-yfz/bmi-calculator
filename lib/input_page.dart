1249efef788fc507fe32fbf078b2af3618ba8333
aaqib-yfz <aaqibshaheer2001@gmail.com>
2024-07-05 23:27:51 +0500

    enums and all

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,9 +3,16 @@ import 'package:bmi_calculator/icon_colum.dart';
 import 'package:bmi_calculator/reusebale_container.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
+
 const bottomContainerHeight = 80.0;
 const bottomContainerColor = 0xFFEB1555;
-const reusablecontColor = 0xFF1D1E33;
+const reusablecontColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
+enum Gender {
+  male,
+  female,
+}
+
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
 
@@ -14,42 +21,87 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColor = inactiveCardColor;
+  Color femaleCardColor = inactiveCardColor;
+
+  void updateColour(Gender selectedGender) {
+    if (selectedGender == Gender.male) {
+      if (maleCardColor == inactiveCardColor) {
+        maleCardColor = reusablecontColor;
+      }
+    } else {
+      maleCardColor = inactiveCardColor;
+    }
+
+    if (selectedGender == Gender.female) {
+      if (femaleCardColor == inactiveCardColor) {
+        femaleCardColor = reusablecontColor;
+      }
+    } else {
+      femaleCardColor = inactiveCardColor;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Center(child: Text('BMI CALCULATOR')),
         ),
-        body:  Column(
+        body: Column(
           children: <Widget>[
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: reusablecont(
-                    colour:Color(reusablecontColor),
-                    cardChild: iconColumn( iconData: FontAwesomeIcons.mars, textData: 'MALE',),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColour(Gender.male);
+                        print('Male was pressed');
+                      });
+                    },
+                    child: reusablecont(
+                      colour: maleCardColor,
+                      cardChild: iconColumn(
+                        iconData: FontAwesomeIcons.mars,
+                        textData: 'MALE',
+                      ),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: reusablecont(colour:Color(reusablecontColor),cardChild: iconColumn(iconData: FontAwesomeIcons.venus, textData: 'FEMALE'),),
-                )
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          updateColour(Gender.female);
+                        });
+                      },
+                  child: reusablecont(
+                    colour: femaleCardColor,
+                    cardChild: iconColumn(
+                        iconData: FontAwesomeIcons.venus, textData: 'FEMALE'),
+                  ),
+                  
+                ))
               ],
             )),
             Expanded(
-              child: Expanded(
-                child: Container(
-                  color: Color(reusablecontColor),
-                  margin: const EdgeInsets.all(15.0),
-                ))
-            ),
+                child: Expanded(
+                    child: Container(
+              color: reusablecontColor,
+              margin: const EdgeInsets.all(15.0),
+            ))),
             Expanded(
                 child: Row(
               children: <Widget>[
                 Expanded(
-                  child: reusablecont(colour:Color(reusablecontColor),cardChild: Column()),
+                  child: reusablecont(
+                      colour: reusablecontColor, cardChild: Column()),
                 ),
-                Expanded(child: reusablecont(colour:Color(reusablecontColor),cardChild: Column())),
+                Expanded(
+                    child: reusablecont(
+                        colour: reusablecontColor, cardChild: Column())),
               ],
             )),
             Container(
@@ -59,9 +111,6 @@ class _InputPageState extends State<InputPage> {
               height: bottomContainerHeight,
             )
           ],
-        
         ));
   }
 }
-
-
